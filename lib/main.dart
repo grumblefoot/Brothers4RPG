37566f3aafa63fd51f9633d43815546a260f9329
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2023-03-07 22:00:23 -0800

    Split pages off into their own classes and refactored to use a set for favorites storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:brothers4_rpg/sheets/generator_page.dart';
+import 'package:brothers4_rpg/sheets/favorites_page.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
+//runs the app
 void main() {
   runApp(MyApp());
 }
 
+//The app object
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Brothers4RPG',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
@@ -25,7 +30,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//This handles the intractable elements, such as buttons and updates the screen
 class MyAppState extends ChangeNotifier {
+
+  //word pair logic
   var current = WordPair.random();
 
   void getNext() {
@@ -33,7 +41,7 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  var favorites = <WordPair>[];
+  var favorites = <WordPair>{};
 
   void toggleFavorite() {
     if (favorites.contains(current)) {
@@ -108,106 +116,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
 
-    if(appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
 
-    return ListView(
-      children: [
-        Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text('You have '
-                '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
 
-}
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-
-        child: Text(
-            pair.asLowerCase,
-            style: style,
-            semanticsLabel: pair.asPascalCase,
-        ),
-      ),
-    );
-
-  }
-}
